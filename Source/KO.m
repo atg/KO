d417d74289f354b647bb47623886405d36c21e7c
Alex Gordon <alextgordon@gmail.com>
2011-07-22 19:27:41 +0100

    I think its working!

--- Source/KO.m
+++ Source/KO.m
@@ -1,5 +1,9 @@
 #import <Cocoa/Cocoa.h>
-#import <dispatch.h>
+#import <dispatch/dispatch.h>
+
+void KORegister();
+BOOL KOPrompt(NSArray *crashLogs);
+void KOSubmit(NSURL *crashLog);
 
 // Call this in -applicationDidFinishLaunching:
 // Scans over KO_CRASH_LOG_DIRECTORY for crash logs
@@ -15,8 +19,13 @@ void KORegister() {
 		return;
 	}
 	
+	// Look in preferences to see if we should prompt
+	__block NSInteger shouldPrompt = [[NSUserDefaults standardUserDefaults] integerForKey:@"KOShouldPrompt"];
+	if (shouldPrompt <= -1)
+		return;
+	
 	// Otherwise...
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 4), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW), ^{
+	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 4), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
 		NSString *directory = [KO_CRASH_LOG_DIRECTORY stringByExpandingTildeInPath];
 		
 		// Look in ~/Library/Logs/DiagnosticReports
@@ -40,7 +49,7 @@ void KORegister() {
 			if (![attrs count])
 				continue;
 			
-			NSDate *dcreated = [attrs objectForKey:];
+			NSDate *dcreated = [attrs objectForKey:NSFileCreationDate];
 			if (!dcreated)
 				continue;
 			
@@ -58,16 +67,20 @@ void KORegister() {
 		if (![viablePaths count])
 			return;
 		
-		// Look in preferences to see if we should prompt
-		BOOL shouldPrompt = ...;
+		// shouldPrompt == -1: never prompt, don't submit
+		// shouldPrompt == 0: prompt
+		// shouldPrompt == 1: never prompt, submit automatically
 		
 		// OK then, let's prompt
-		if (shouldPrompt && !KOPrompt(viablePaths))
+		if (shouldPrompt == 0 && !KOPrompt(viablePaths))
 			return;
+		
+		// We don't want to prompt next time!
+		shouldPrompt = 1;
 		
 		// We're still here. Excellent!
 		for (NSString *crashLog in viablePaths) {
-			KOSubmit(crashLog);
+			KOSubmit([NSURL fileURLWithPath:crashLog isDirectory:NO]);
 		}
 	});
 	
@@ -76,13 +89,69 @@ void KORegister() {
 
 // Prompts the user for permission to submit the crash log, unless they've already given permission
 BOOL KOPrompt(NSArray *crashLogs) {
+	__block BOOL shouldSend = YES;
+	
 	// We also want to record if we should prompt in future
 	dispatch_sync(dispatch_get_main_queue(), ^{
 		
+		NSInteger r = NSRunAlertPanel(@"Chocolat appears to have crashed last time it was launched. Would you like to send a problem report to the developers?", @"Send", @"Don't Send'", @"Always Send", @"Problem reports that have been sent to Apple are not passed on to developers.");
+		
+		if (r == NSAlertDefaultReturn) {
+			// Send
+			
+		}
+		else if (r == NSAlertOtherReturn) {
+			// Always send
+			[[NSUserDefaults standardUserDefaults] setInteger:1 forKey:@"KOShouldPrompt"];
+		}
+		else {
+			// Don't send
+			shouldSend = NO;
+		}		
 	});
+	
+	return shouldSend;
 }
 
 // Submits the crash log at `crashLog` to the server
 void KOSubmit(NSURL *crashLog) {
+    NSURL *url = [NSURL URLWithString:[[[NSBundle mainBundle] infoDictionary] valueForKey:@"KOCrashReportServerURL"]];
+    
+	NSString *crashLogString = [NSString stringWithContentsOfURL:crashLog usedEncoding:NULL error:NULL];
 	
+	NSMutableDictionary *postItems = [NSMutableDictionary dictionary];
+	[postItems setValue:@"crash" forKey:@"type"];
+	[postItems setValue:[[NSRunningApplication currentApplication] localizedName] forKey:@"app"];
+	[postItems setValue:[[[NSBundle mainBundle] infoDictionary] valueForKey:@"CHBundleVersion"] forKey:@"version"];
+	[postItems setValue:[NSNumber numberWithInteger:(NSInteger)[[NSDate date] timeIntervalSince1970]] forKey:@"date"];
+	[postItems setValue:crashLogString forKey:@"blob"];
+	
+	NSMutableURLRequest *request = [[[NSMutableURLRequest alloc] init] autorelease];
+	[request setURL:url];
+	
+	//POST
+	NSMutableString *postString = [NSMutableString string];
+	
+	int i = 0;
+	int postItemsCount = [postItems count];
+	for (id key in postItems)
+	{
+		NSString *encodedValue = [NSMakeCollectable(CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (CFStringRef)[postItems valueForKey:key], NULL, CFSTR("?=&+"), kCFStringEncodingUTF8)) autorelease];
+
+		[postString appendFormat:@"%@=%@", key, encodedValue];
+		
+		if (i < postItemsCount - 1)
+			[postString appendString:@"&"];
+		i++;
+	}
+	
+	
+	NSData *postData = [postString dataUsingEncoding:NSUTF8StringEncoding];
+	NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
+	
+	[request setHTTPMethod:@"POST"];
+	[request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+	[request setHTTPBody:postData];
+	
+	[NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
 }
